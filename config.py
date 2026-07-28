29b919b87ff5b27757379b17e748d0b9ee1eb9fb
zanyarmzurii <zanyarkurdi2008@gmail.com>
2026-07-28 18:59:35 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+# 🗝️ Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8984267002:AAFgnVwZ9IuesAqXCUfzFiK-Wv24ilvyihc")
+
+# 👤 Admin Details
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7296733212"))
+ADMIN_PHONE = "+964 750 604 5491"
+ADMIN_USERNAME = "@z_14x"
+ADMIN_LINK = "https://t.me/z_14x"
+
+# 💳 Payment Methods
+FASTPAY_NUMBER = "+964 750 604 5491"
+FIB_NUMBER = "+964 750 604 5491"
+TON_WALLET = "UQAd90pxOXqVTZ04Nv6c2F1FCfd3dVJGxCrnuCm_6Mavz5Ua"
+USDT_TRC20 = "TKUfVwnjyT2KUa9xnBreT32YLLJEwACHpc"
+
+# 🌍 10 Languages
+LANGUAGES = {
+    "en": "🇬🇧 ئینگلیزی",
+    "ar": "🇸🇦 عەرەبی",
+    "tr": "🇹🇷 تورکی",
+    "de": "🇩🇪 ئەلمانی",
+    "fr": "🇫🇷 فەرەنسی",
+    "es": "🇪🇸 ئسپانی",
+    "ru": "🇷🇺 ڕووسی",
+    "fa": "🇮🇷 فارسی",
+    "it": "🇮🇹 ئیتالی",
+    "zh": "🇨🇳 چینی"
+}
