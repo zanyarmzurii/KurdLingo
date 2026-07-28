12f5c47d4140a2e5659aa432e3adf31e6a405a17
zanyarmzurii <zanyarkurdi2008@gmail.com>
2026-07-28 21:10:46 +0300

    Create __init__.py

---
+++ bot/services/__init__.py
@@ -0,0 +1,13 @@
+from .lesson_service import LessonService
+from .quiz_service import QuizService
+from .user_service import UserService
+from .certificate_service import CertificateService
+from .notification_service import NotificationService
+
+__all__ = [
+    "LessonService",
+    "QuizService",
+    "UserService",
+    "CertificateService",
+    "NotificationService",
+]
