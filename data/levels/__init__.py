36bd03f9d9a706200badc2608e2636c6a6741711
zanyarmzurii <zanyarkurdi2008@gmail.com>
2026-07-28 21:26:16 +0300

    Create __init__.py

---
+++ data/levels/__init__.py
@@ -0,0 +1,9 @@
+from .beginner import BEGINNER
+from .intermediate import INTERMEDIATE
+from .advanced import ADVANCED
+
+LEVELS = {
+    "beginner": BEGINNER,
+    "intermediate": INTERMEDIATE,
+    "advanced": ADVANCED,
+}
