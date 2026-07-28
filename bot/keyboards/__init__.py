d8b96d8aa83dffb89c38a50baeefab5196f0faf9
zanyarmzurii <zanyarkurdi2008@gmail.com>
2026-07-28 21:04:12 +0300

    Create __init__.py

---
+++ bot/keyboards/__init__.py
@@ -0,0 +1,11 @@
+from .main_menu import main_menu_keyboard
+from .language_select import language_select_keyboard
+from .plans import plans_keyboard, plan_duration_keyboard, payment_method_keyboard
+
+__all__ = [
+    "main_menu_keyboard",
+    "language_select_keyboard",
+    "plans_keyboard",
+    "plan_duration_keyboard",
+    "payment_method_keyboard",
+]
